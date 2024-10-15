58702466ba2b3913fd86a5e63bb09925b14422e5
hengquserbot <165480619+hengquserbot@users.noreply.github.com>
2024-10-15 19:07:19 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -1,11 +1,11 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "22016511"))
-API_HASH = os.getenv("API_HASH", "8267335b1cbd700a179dbe9a9a482d1e")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7498928789:AAGn8R-TqA98cIK--9pQQnMHkkPMI_r0lzw")
-OWNER_ID = int(os.getenv("OWNER_ID", "1846313396"))
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002448404141"))
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002109872719 -1002058863067 -1001986858575 -1001287188817 -1002226843410").split()))
+API_ID = int(os.getenv("API_ID", "17855094"))
+API_HASH = os.getenv("API_HASH", "004a12ddadfd7e96afa84af7f37e4fe6")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7498928789:7408701168:AAHcOTjV2ob4AvqWW5VNZHa72N6oHIbZEPk")
+OWNER_ID = int(os.getenv("OWNER_ID", "17855094"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "--1002369567634"))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002109872719 ").split()))
 MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
 AI_GOOGLE_API = os.getenv("AI_GOOGLE_API", "AIzaSyAM4A7L0Qj3loDZDupt0X74PDne6Tx2YLA")
@@ -18,6 +18,5 @@ START_IMG_URL = os.getenv(
 )
 
 DEVS = [
-    6953052196, #devsganteng
-    1846313396
+    17855094, #devscool
 ]
