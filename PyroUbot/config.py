dd655f1594b32a5cbe0582e996990f1027d4e866
hengquserbot <165480619+hengquserbot@users.noreply.github.com>
2024-10-18 03:05:44 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -8,10 +8,10 @@ LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "--1002369567634"))
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002109872719 ").split()))
 MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
-AI_GOOGLE_API = os.getenv("AI_GOOGLE_API", "AIzaSyAM4A7L0Qj3loDZDupt0X74PDne6Tx2YLA")
+AI_GOOGLE_API = os.getenv("AI_GOOGLE_API", "#AIzaSyAM4A7L0Qj3loDZDupt0X74PDne6Tx2YLA")
 MONGO_URL = os.getenv(
     "MONGO_URL",
-    "mongodb+srv://Japran:memek@cluster0.bvzu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://ceyycantik05xlmghbnj@cluster0.t2jqp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 )
 START_IMG_URL = os.getenv(
     "START_IMG_URL", "https://itzpire.com/file/93496dbb7582.jpg"
